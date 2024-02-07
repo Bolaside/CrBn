1564e251423518c195eb2e0e342bc786ff52e95c
Oopaz <bolaside@gmail.com>
2024-02-07 19:17:00 +0200

    Added custom RPC

--- source/index.js
+++ source/index.js
@@ -1,1 +1,22 @@
-console.log("h")
+const { ActivityType, Client, Events, GatewayIntentBits } = require("discord.js")
+const DotEnv = require("dotenv")
+
+DotEnv.config()
+
+if (!process.env.TOKEN) {
+  throw new Error("No token provided")
+}
+
+const crbn = new Client({ intents: 32767 })
+
+crbn.once(Events.ClientReady, client => {
+  console.log(`INFO: ${client.user.tag} is ready!`)
+
+  client.user.setStatus("dnd")
+  client.user.setActivity("my balls", {
+    state: "your jaws",
+    type: ActivityType.PLAYING
+  })
+})
+
+crbn.login(process.env.TOKEN)
